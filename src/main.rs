0a3f23b9927631026b1f93403fea58a548e35cc8
Yjn024 <jiening.yu@outlook.com>
2023-10-10 15:31:16 +0800

    done without testing :D

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,21 @@
-use std::{fs::File, io::Read};
+use std::{collections::HashMap, fs::File, io::Read};
 
 use chrono::DateTime;
 
 use iced::{
     futures::TryFutureExt,
     theme,
-    widget::{button, horizontal_space, Column, Row, Space, Text},
-    Application, Command, Font, Length,
+    widget::{button, container, vertical_space, Column, Row, Scrollable, Text},
+    Application, Color, Command, Font, Length,
 };
 use iced_aw::Split;
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
 fn main() -> iced::Result {
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::WARN)
+        .init();
+
     let config: Config;
 
     {
@@ -23,12 +27,12 @@ fn main() -> iced::Result {
 
     App::run(iced::Settings {
         window: iced::window::Settings {
-            size: (500, 800),
+            size: (1200, 800),
             ..Default::default()
         },
         default_font: Font::with_name(config.font.to_owned().leak()),
         flags: config,
-        default_text_size: 11.5,
+        default_text_size: 15.0,
         ..Default::default()
     })
 }
@@ -53,9 +57,9 @@ struct BuiltHost {
     paper_need_process: String,
     process_paper: String,
 }
+
 #[derive(Debug)]
 struct StaticIns {
-    config: Config,
     host: BuiltHost,
     client: reqwest::Client,
 }
@@ -63,11 +67,12 @@ struct StaticIns {
 #[derive(Debug)]
 struct App {
     /// Loaded papers.
-    papers: Vec<Paper>,
-
+    papers: HashMap<i32, Paper>,
     static_ins: &'static StaticIns,
 
     split_0_pos: Option<u16>,
+    selected_paper: Option<i32>,
+    nerd_font: Font,
 }
 
 impl Application for App {
@@ -82,7 +87,7 @@ impl Application for App {
     fn new(flags: Self::Flags) -> (Self, iced::Command<Self::Message>) {
         (
             Self {
-                papers: vec![],
+                papers: HashMap::new(),
 
                 static_ins: Box::leak(Box::new(StaticIns {
                     host: BuiltHost {
@@ -96,11 +101,12 @@ impl Application for App {
                         ),
                     },
 
-                    config: flags,
                     client: reqwest::Client::new(),
                 })),
 
-                split_0_pos: None,
+                split_0_pos: Some(200),
+                selected_paper: None,
+                nerd_font: Font::MONOSPACE,
             },
             iced::font::load(include_bytes!("../fonts/SymbolsNerdFontMono-Regular.ttf").as_slice())
                 .map(Msg::FontLoaded),
@@ -136,6 +142,38 @@ impl Application for App {
                     std::convert::identity,
                 )
             }
+            Msg::RefreshDone(papers) => {
+                for paper in papers {
+                    self.papers.insert(paper.pid, paper);
+                }
+            }
+            Msg::OpenPaper(paper) => self.selected_paper = Some(paper),
+            Msg::Accept(paper) => {
+                let si = self.static_ins;
+                return Command::perform(
+                    async move {
+                        let span = tracing::span!(tracing::Level::INFO, "accept paper {paper}");
+                        let _ = span.enter();
+
+                        #[derive(Debug, Serialize)]
+                        struct Req {
+                            pid: i32,
+                        }
+
+                        if let Err(err) = si
+                            .client
+                            .post(&si.host.process_paper)
+                            .json(&Req { pid: paper })
+                            .send()
+                            .await
+                        {
+                            tracing::event!(tracing::Level::ERROR, "{err}");
+                        }
+                    },
+                    |_| Msg::Refresh,
+                );
+            }
+            Msg::FontLoaded(Ok(_)) => self.nerd_font = Font::with_name("Symbols Nerd Font Mono"),
             _ => (),
         }
 
@@ -146,25 +184,131 @@ impl Application for App {
         let mut left = Column::new();
 
         {
-            let mut bar = Row::new().height(13);
+            let mut bar = Row::new().height(30).width(Length::Fill);
 
             bar = bar.push(
-                button(Text::new("󰑐").width(15).height(15))
-                    .style(theme::Button::Text)
-                    .on_press(Msg::Refresh),
+                Text::new("   PAPERS")
+                    .height(30)
+                    .width(Length::Fill)
+                    .horizontal_alignment(iced::alignment::Horizontal::Left)
+                    .vertical_alignment(iced::alignment::Vertical::Center)
+                    .style(Color::new(0.5, 0.5, 0.5, 1.0)),
             );
-            bar = bar.push(horizontal_space(Length::Fill));
+
+            bar = bar.push(
+                button(
+                    Text::new("󰑐")
+                        .width(30)
+                        .height(30)
+                        .size(13.5)
+                        .horizontal_alignment(iced::alignment::Horizontal::Center)
+                        .style(Color::new(0.5, 0.5, 0.5, 1.0))
+                        .font(self.nerd_font),
+                )
+                .style(theme::Button::Text)
+                .on_press(Msg::Refresh),
+            );
 
             left = left.push(bar);
         }
 
-        let right = Space::new(Length::Fill, Length::Fill);
+        {
+            let mut down = Column::new().width(Length::Fill);
+
+            let mut papers: Vec<&Paper> = self.papers.values().collect();
+            papers.sort_unstable_by_key(|paper| &paper.time);
+
+            for paper in papers {
+                down = down.push(
+                    button(
+                        container({
+                            let mut row = Row::new().push(
+                                Text::new(&paper.name)
+                                    .width(Length::Fill)
+                                    .horizontal_alignment(iced::alignment::Horizontal::Left)
+                                    .height(13.5)
+                                    .vertical_alignment(iced::alignment::Vertical::Center),
+                            );
+
+                            row = match paper.processed {
+                                Some(true) => row.push(
+                                    Text::new("󰄬")
+                                        .size(10)
+                                        .width(15)
+                                        .font(self.nerd_font)
+                                        .style(Color::new(
+                                            0.411_764_7,
+                                            0.694_117_67,
+                                            0.325_490_2,
+                                            0.003_921_569,
+                                        )),
+                                ),
+                                Some(false) => row.push(
+                                    Text::new("󰅖")
+                                        .size(10)
+                                        .width(15)
+                                        .font(self.nerd_font)
+                                        .style(Color::new(
+                                            0.772_549_03,
+                                            0.352_941_2,
+                                            0.396_078_44,
+                                            0.003_921_569,
+                                        )),
+                                ),
+                                None => row,
+                            };
+
+                            row
+                        })
+                        .style(
+                            if self.selected_paper.map_or(false, |e| paper.pid == e) {
+                                theme::Container::Box
+                            } else {
+                                theme::Container::Transparent
+                            },
+                        ),
+                    )
+                    .style(theme::Button::Text)
+                    .on_press(Msg::OpenPaper(paper.pid)),
+                )
+            }
+
+            left = left.push(Scrollable::new(down).height(Length::Fill));
+        }
+
+        let mut right = Column::new().height(Length::Fill).width(Length::Fill);
+        if let Some(paper) = self
+            .selected_paper
+            .and_then(|value| self.papers.get(&value))
+        {
+            right = right
+                .push(vertical_space(15))
+                .push(container(Text::new(&paper.info)).width(Length::Fill))
+                .push(vertical_space(15));
+
+            right = right
+                .push(Text::new(format!("Time: {}", paper.time.to_rfc3339())))
+                .push(Text::new(format!("Name: {}", paper.name)))
+                .push(Text::new(format!("Email: {}", paper.email)))
+                .push(vertical_space(Length::Fill));
+
+            if paper.processed.is_some() {
+                right = right.push(
+                    button(
+                        Text::new("Accept")
+                            .horizontal_alignment(iced::alignment::Horizontal::Center),
+                    )
+                    .width(Length::Fill)
+                    .on_press(Msg::Accept(paper.pid)),
+                );
+            }
+        }
 
         Split::new(
             left,
             right,
             self.split_0_pos,
-            iced_aw::split::Axis::Horizontal,
+            iced_aw::split::Axis::Vertical,
             Msg::Split0Resized,
         )
         .into()
@@ -177,6 +321,8 @@ enum Msg {
     Split0Resized(u16),
     Refresh,
     RefreshDone(Vec<Paper>),
+    OpenPaper(i32),
+    Accept(i32),
 }
 
 #[derive(Debug, Deserialize, Clone)]
@@ -186,7 +332,8 @@ struct Paper {
     time: DateTime<chrono::Local>,
     name: String,
     email: String,
+    color: String,
 
     #[serde(default)]
-    processed: bool,
+    processed: Option<bool>,
 }
